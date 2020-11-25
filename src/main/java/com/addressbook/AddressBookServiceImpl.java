073d1f1048714c22f32b2dd1ff8c9ea2c5be0568
Fernando <yahirokendy@gmail.com>
2020-11-24 20:26:59 -0400

    Criado a parte do tipo: Bidirecional e Client Streaming

--- src/main/java/com/addressbook/AddressBookServiceImpl.java
+++ src/main/java/com/addressbook/AddressBookServiceImpl.java
@@ -27,4 +27,66 @@ public class AddressBookServiceImpl extends AddressBookServiceGrpc.AddressBookSe
 
         responseObserver.onCompleted();
     }
+
+    public StreamObserver<ContactRequest> addListContact(StreamObserver<ContactResponse> responseObserver){
+        StreamObserver<ContactRequest> requestObserver = new StreamObserver<ContactRequest>() {
+
+            String result = "Hello  \n";
+            @Override
+            public void onNext(ContactRequest value) {
+                result += value.getName() + "\n";
+                // ContactResponse response = ContactResponse.newBuilder()
+                //         .setResult(result)
+                //         .setCode("200")
+                //         .build();
+
+                // responseObserver.onNext(response);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                // client is done
+                responseObserver.onNext(
+                    ContactResponse.newBuilder()
+                                .setResult(result)
+                                .build()
+                );
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestObserver;
+    }
+
+    @Override
+    public StreamObserver<ContactRequest> addMultipleContact(StreamObserver<ContactResponse> responseObserver) {
+        StreamObserver<ContactRequest> requestObserver = new StreamObserver<ContactRequest>() {
+            @Override
+            public void onNext(ContactRequest value) {
+                String result = "Hello " + value.getName();
+                ContactResponse multipleResponse = ContactResponse.newBuilder()
+                        .setResult(result)
+                        .build();
+
+                responseObserver.onNext(multipleResponse);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestObserver;
+    }
 }
